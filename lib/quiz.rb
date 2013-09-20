a2034e45170557853233bc1b89e298c7b1b3bc75
Giovanni Intini <intinig@gmail.com>
2013-09-20 16:41:12 +0200

    added reverse region question

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -10,6 +10,7 @@ class Quiz
     denominations = options[:denominations] || ["DOC", "DOCG"]
     easy_by_region = options[:easy_by_region]
     excluded_ids = options[:excluded_ids]
+    grapes_limit = 6
 
     denominations = Denomination.all(:name => denominations)
     wines = Wine.all(:denomination => denominations, :id.not => excluded_ids)
@@ -20,19 +21,21 @@ class Quiz
 
     excluded_ids = wines.map {|w| w.id}
 
-    new_wines = wines.dup
-
     unless easy_by_region
-      wines.each do |wine|
-        new_wines.delete(wine) if wine.name =~ /#{wine.region.name}/i
+      wines.delete_if do |wine|
+        wine.name =~ /#{wine.region.name}/i
       end
     end
 
-    if new_wines.size < n
-      new_wines << sample_wines(n - wines.size, options.merge(:excluded_ids => excluded_ids))
+    wines.delete_if do |wine|
+      wine.grapes.size > 5
     end
 
-    new_wines.flatten
+    if wines.size < n
+      wines << sample_wines(n - wines.size, options.merge(:excluded_ids => excluded_ids))
+    end
+
+    wines.flatten
   end
 
   def self.build_question(question, correct_answer, incorrect_answers, correct_message, incorrect_message)
@@ -51,7 +54,7 @@ class Quiz
   end
 
   def self.region_question(n = 1)
-    wines = sample_wines(n, :denominations => ["DOCG"])
+    wines = sample_wines(n, :denominations => ["DOCG", "DOC", "DOP"])
     questions = []
     wines.each do |wine|
       questions << build_question(
@@ -65,12 +68,13 @@ class Quiz
     questions
   end
 
-  # C'e' un bug: a volte tira fuori una domanda con due risposte uguali
-  # Es. Vermentino di Gallura
   # TODO: reverse questions: Che vino viene fatto con queste uve? Docg di questa regione?
 
   def self.build_grapes_question_with_wine(wine)
-    incorrect_answers = Wine.all(:name.not => wine.name, :grapes.not => wine.grapes, :denomination => wine.denomination).map {|w| w.grapes.map {|g| g.name}.join(", ")}.uniq.sample(3)
+    incorrect_answers = Wine.all(:name.not => wine.name, :grapes.not => wine.grapes, :denomination => wine.denomination).select do |w|
+      w if w.grapes.size < 6
+    end.map {|w| w.grapes.map {|g| g.name}.join(", ")}.uniq.sample(3)
+
     correct_answer = wine.grapes.map {|g| g.name}.join(", ")
 
     build_question(
@@ -91,14 +95,36 @@ class Quiz
     questions
   end
 
-  def self.random_question(n = 1)
+  def self.build_reverse_region_question_with_region(region)
+    denominations = Denomination.all :name => ["DOC", "DOCG", "DOP"]
+    incorrect_answers = Wine.all(:region => region, :denomination => denominations).sample(3).map{|w| w.name}
+    correct_answer = Wine.all(:region.not => region, :denomination => denominations).sample(1).map{|w| w.name}.first
+
+    build_question(
+      "Quali di questi vini non appartiene alla regione #{region.name}?",
+      correct_answer,
+      incorrect_answers,
+      "<p><span>Corretto!</span></p>",
+      "<p><span>Sbagliato!</span> L'intruso è #{correct_answer}!</p>"
+      )
+  end
+
+  def self.reverse_region_question(n = 1)
+    regions = Region.all.sample(n)
     questions = []
-    n.times do |i|
-      if rand(2).to_i > 0
-        questions << region_question
-      else
-        questions << grapes_question
-      end
+    regions.each do |region|
+      questions << build_reverse_region_question_with_region(region)
+    end
+    questions
+  end
+
+  # quali di queste uve non e' in questo vino?
+
+  def self.random_question(n = 1)
+    question_types = [Proc.new{region_question}, Proc.new{reverse_region_question}, Proc.new{grapes_question}]
+    questions = []
+    n.times do
+      questions << question_types.sample.call
     end
     questions.flatten
   end
