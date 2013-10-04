9480f0b7dafe2796f153787df259598af6721cd0
Giovanni Intini <intinig@gmail.com>
2013-10-04 12:42:53 +0200

    cleaning a little bit

--- lib/models/wine.rb
+++ lib/models/wine.rb
@@ -21,61 +21,61 @@ class Wine
     Denomination.first(:name => "DOCG").wines.all(params)
   end
 
-  def self.random(n = 0, options = {})
-    denominations = options[:denominations] || ["DOC", "DOCG"]
-    easy_by_region = options[:easy_by_region]
-    exclude_grape_wines = options[:exclude_grape_wines]
-    excluded_ids = options[:excluded_ids]
-    upper_grapes_limit = options[:upper_grapes_limit] || 6
-    lower_grapes_limit = options[:lower_grapes_limit] || 0
+  # def self.random(n = 0, options = {})
+  #   denominations = options[:denominations] || ["DOC", "DOCG"]
+  #   easy_by_region = options[:easy_by_region]
+  #   exclude_grape_wines = options[:exclude_grape_wines]
+  #   excluded_ids = options[:excluded_ids]
+  #   upper_grapes_limit = options[:upper_grapes_limit] || 6
+  #   lower_grapes_limit = options[:lower_grapes_limit] || 0
 
-    denominations = Denomination.all(:name => denominations)
-    wines = all(:denomination => denominations, :id.not => excluded_ids)
+  #   denominations = Denomination.all(:name => denominations)
+  #   wines = all(:denomination => denominations, :id.not => excluded_ids)
 
-    n = n > wines.count ? wines.count : n
+  #   n = n > wines.count ? wines.count : n
 
-    wines = wines.sample(n) if n > 0
+  #   wines = wines.sample(n) if n > 0
 
-    excluded_ids = wines.map {|w| w.id}
+  #   excluded_ids = wines.map {|w| w.id}
 
-    unless easy_by_region
-      wines.delete_if do |wine|
-        wine.name =~ /#{wine.region.name}/i
-      end
-    end
+  #   unless easy_by_region
+  #     wines.delete_if do |wine|
+  #       wine.name =~ /#{wine.region.name}/i
+  #     end
+  #   end
 
-    if exclude_grape_wines
-      new_wines = wines.dup
-      wines.each do |w|
-        w.grapes.map{|g| g.name.downcase}.each do |grape|
-          if w.name =~ /#{grape}/i
-            new_wines.delete w
-            break
-          end
-        end
-      end
-      wines = new_wines
-    end
+  #   if exclude_grape_wines
+  #     new_wines = wines.dup
+  #     wines.each do |w|
+  #       w.grapes.map{|g| g.name.downcase}.each do |grape|
+  #         if w.name =~ /#{grape}/i
+  #           new_wines.delete w
+  #           break
+  #         end
+  #       end
+  #     end
+  #     wines = new_wines
+  #   end
 
 
-    if upper_grapes_limit != 0
-      wines.delete_if do |wine|
-        wine.grapes.size > upper_grapes_limit
-      end
-    end
+  #   if upper_grapes_limit != 0
+  #     wines.delete_if do |wine|
+  #       wine.grapes.size > upper_grapes_limit
+  #     end
+  #   end
 
-    if lower_grapes_limit > 0
-      wines.delete_if do |wine|
-        wine.grapes.size < lower_grapes_limit
-      end
-    end
+  #   if lower_grapes_limit > 0
+  #     wines.delete_if do |wine|
+  #       wine.grapes.size < lower_grapes_limit
+  #     end
+  #   end
 
-    if wines.size < n
-      wines << random(n - wines.size, options.merge(:excluded_ids => excluded_ids))
-    end
+  #   if wines.size < n
+  #     wines << random(n - wines.size, options.merge(:excluded_ids => excluded_ids))
+  #   end
 
-    wines.flatten
-  end
+  #   wines.flatten
+  # end
 
   def stripped_name
     name.gsub(denomination.name, "").strip
--- spec/wine_spec.rb
+++ spec/wine_spec.rb
@@ -3,45 +3,45 @@ require_relative '../lib/models'
 require_relative '../lib/utilities'
 
 describe Wine do
-  describe "random" do
-    def default_options(options = {})
-      {
-        :easy_by_region => true,
-        :denominations => ["DOC", "DOCG", "DOP", "IGP"],
-        :upper_grapes_limit => 0,
-        :lower_grapes_limit => 0
-      }.merge(options)
-    end
-    it "does not bomb out if you ask more wines than possible" do
-      Wine.random(100000, default_options).size.should == Wine.count
-    end
+  # describe "random" do
+  #   def default_options(options = {})
+  #     {
+  #       :easy_by_region => true,
+  #       :denominations => ["DOC", "DOCG", "DOP", "IGP"],
+  #       :upper_grapes_limit => 0,
+  #       :lower_grapes_limit => 0
+  #     }.merge(options)
+  #   end
+  #   it "does not bomb out if you ask more wines than possible" do
+  #     Wine.random(100000, default_options).size.should == Wine.count
+  #   end
 
-    it "gives everything if you tell it to return 0" do
-      Wine.random(0, default_options).size.should == Wine.count
-    end
+  #   it "gives everything if you tell it to return 0" do
+  #     Wine.random(0, default_options).size.should == Wine.count
+  #   end
 
-    it "gives only DOC and DOCG wines by default" do
-      Wine.random(0, default_options(:denominations => nil)).size.should == Wine.count(:denomination => Denomination.all(:name => ["DOC", "DOCG"]))
-    end
+  #   it "gives only DOC and DOCG wines by default" do
+  #     Wine.random(0, default_options(:denominations => nil)).size.should == Wine.count(:denomination => Denomination.all(:name => ["DOC", "DOCG"]))
+  #   end
 
-    it "should exclude wines that contain a region name" do
-      easy_wines = Wine.random(0, default_options(:denominations => nil))
-      complex_wines = Wine.random(0, default_options(:easy_by_region => false, :denominations => nil))
-      (easy_wines.size - complex_wines.size).should == SommQuiz::Utilities.count_wines_with_region_in_their_name
-    end
+  #   it "should exclude wines that contain a region name" do
+  #     easy_wines = Wine.random(0, default_options(:denominations => nil))
+  #     complex_wines = Wine.random(0, default_options(:easy_by_region => false, :denominations => nil))
+  #     (easy_wines.size - complex_wines.size).should == SommQuiz::Utilities.count_wines_with_region_in_their_name
+  #   end
 
-    it "excludes wines that contain the name of the grape" do
-      easy_wines = Wine.random(0, default_options(:denominations => nil))
-      complex_wines = Wine.random(0, default_options(:exclude_grape_wines => true, :denominations => nil))
-      (easy_wines.size - complex_wines.size).should == SommQuiz::Utilities.count_wines_with_grapes_in_their_name
-    end
+  #   it "excludes wines that contain the name of the grape" do
+  #     easy_wines = Wine.random(0, default_options(:denominations => nil))
+  #     complex_wines = Wine.random(0, default_options(:exclude_grape_wines => true, :denominations => nil))
+  #     (easy_wines.size - complex_wines.size).should == SommQuiz::Utilities.count_wines_with_grapes_in_their_name
+  #   end
 
-    it "always gives a sufficient number of wines" do
-      wines = Wine.random(20,
-        :denominations => ["DOCG", "DOC"],
-        :lower_grapes_limit => 3
-        )
-      wines.size.should == 20
-    end
-  end
+  #   it "always gives a sufficient number of wines" do
+  #     wines = Wine.random(20,
+  #       :denominations => ["DOCG", "DOC"],
+  #       :lower_grapes_limit => 3
+  #       )
+  #     wines.size.should == 20
+  #   end
+  # end
 end
