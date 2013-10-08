402bd87784613120ebfd24524f6c1f4044c55bb0
Giovanni Intini <intinig@gmail.com>
2013-10-08 17:57:58 +0200

    fixed html bug in js code

--- public/js/master.js
+++ public/js/master.js
@@ -9,7 +9,7 @@ $(function () {
       "info": {
         "name":    "Il Quiz del Sommelier",
         "main":    "<p>Pensi di essere pronto a diventare un Sommelier? Metti alla prova la tua conoscenza del mondo dei vini.</p>",
-        "results": "<h5>Quiz del Sommelier</h5><p>Il Quiz del Sommelier nasce per divertimento nella fucina di idee di <a href=\"http://www.mikamai.com\">MIKAMAI, per aiutare tutti coloro che vogliono mettere alla prova la propria conoscenza del meraviglioso, e vasto, mondo del vino. Tutte le domande sono generate a caso, e tutti i vini che abbiamo bevuto durante lo sviluppo sono gentilmente offerti da <a href=\"http://www.vinodalproduttore.it\">Vinodalproduttore.it</a>, che seleziona per voi i migliori vini italiani. Segnalate imperfezioni o commenti a giovanni CHIOCCIOLA mikamai.com</p>",
+        "results": "<h5>Quiz del Sommelier</h5><p>Il Quiz del Sommelier nasce per divertimento nella fucina di idee di <a href=\"http://www.mikamai.com\">MIKAMAI</a>, per aiutare tutti coloro che vogliono mettere alla prova la propria conoscenza del meraviglioso, e vasto, mondo del vino. Tutte le domande sono generate a caso, e tutti i vini che abbiamo bevuto durante lo sviluppo sono gentilmente offerti da <a href=\"http://www.vinodalproduttore.it\">Vinodalproduttore.it</a>, che seleziona per voi i migliori vini italiani. Segnalate imperfezioni o commenti a giovanni CHIOCCIOLA mikamai.com</p>",
         "level1":  "Sommelier",
         "level2":  "Aspirante Sommelier",
         "level3":  "Bevitore volenteroso",
